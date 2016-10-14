31f2e6cd6e52752bbcd78e9bddbb3298f27f3d33
Spencer Tranter <spencertranter95@gmail.com>
2016-10-14 20:00:55 +0000

    Allow users to delete reviews they have made

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,24 @@
 class ReviewsController < ApplicationController
-  # def new
-  #   @review = Review.new
-  #   @product = product.find(params[:product_id])
-  # end
+  before_filter :authorize
+
   def create
     puts review_params
+    @product = Product.find(params["product_id"])
     @review = Review.new(review_params)
-    @review.product_id = params["product_id"] ## GET SECOND OPINION
+    @review.product_id = params["product_id"]
+    @review.user_id = @current_user.id
+    @review.rating = params["rating"].to_i ## GET SECOND OPINION
     if @review.save
       redirect_to :back
     else
-      # redirect_to home,
-      # error: order.errors.full_messages.first
       puts @review.errors.to_a
     end
+  end
+
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to :back, notice: 'Product deleted!'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show] do
     resources :reviews, only: [:create]
   end
+  resources :reviews, only: [:destroy]
 
   resources :users, only: [:new, :create]
   get '/login' => 'sessions#new'
