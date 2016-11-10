6290579349e60fad55c107d4baec09c2fb013935
Patrick Simonian <patricksimonian@gmail.com>
2016-11-10 12:55:45 -0800

    add first/last name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110205112) do
+ActiveRecord::Schema.define(version: 20161110205510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20161110205112) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_foreign_key "line_items", "orders"
