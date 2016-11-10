e739142aa1ac4d7149f6fea7f0ea626fa2729788
Patrick Simonian <patricksimonian@gmail.com>
2016-11-10 13:51:46 -0800

    added some basic validations for signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence:true
+  validates :password, length: {minimum: 8}
+  validate :password_confirmed?
 
-  validate
+  private
+
+  def password_confirmed?
+    password == confirm_password
+  end
 end
+
+
+# need email validator
