53f5a3074ad670e14fd74e974666b54048cc2977
Patrick Simonian <patricksimonian@gmail.com>
2016-11-10 11:43:06 -0800

    intial commit for admin dashboard add-categories-feature

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -34,7 +35,7 @@ Rails.application.routes.draw do
 
   # Example resource route with options:
   #   resources :products do
-  #     member do
+  #       member do
   #       get 'short'
   #       post 'toggle'
   #     end
