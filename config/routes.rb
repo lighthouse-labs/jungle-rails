5b0da3d516032782c3cf4992d58ab669fc78291c
Patrick Simonian <patricksimonian@gmail.com>
2016-11-10 12:31:04 -0800

    implement edit feature for categories, added styling located at admin/categories.scss

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new]
+    resources :categories, only: [:index, :new, :create, :edit]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
