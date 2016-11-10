84999169887f7c5992fc1af16828d39f96309033
Patrick Simonian <patricksimonian@gmail.com>
2016-11-10 14:26:50 -0800

    signup feature works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
-      redirect_to '/signup'
+      render 'new'
     end
   end
+
   private
 
   def user_params
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
       :first_name,
       :last_name,
       :password,
-      :confirm_password,
+      :password_confirmation,
       :email
     )
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
+
 class User < ActiveRecord::Base
+  # has secure password validates password confirmation, presence
+  #  and max length of 72 char
   has_secure_password
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence:true
   validates :password, length: {minimum: 8}
-  validate :password_confirmed?
 
-  private
-
-  def password_confirmed?
-    password == confirm_password
-  end
 end
 
 
