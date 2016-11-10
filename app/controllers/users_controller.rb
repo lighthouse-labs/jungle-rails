e930bb6db0e872ea223cbffe75cf51ec52382343
Andreas Hart <andreastheohart@gmail.com>
2016-11-10 22:02:48 +0000

    looking

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      respond_to do |format|
+        if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.thank_you(@user).deliver_later
+
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
+    end
+
+
+
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
+
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,9 @@
+class ApplicationMailer < ActionMailer::Base
+default from: 'notifications@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
