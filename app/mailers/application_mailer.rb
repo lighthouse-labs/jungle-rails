f5dedb442c09bd954aefee2bab34eaa79c03dc2c
Andreas Hart <andreastheohart@gmail.com>
2016-11-10 23:47:43 +0000

    email confirmation complete

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-default from: 'notifications@example.com'
-
-  def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
+default from: "no-reply@jungle.com"
+  layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'no-reply@jungle.com'
+
+  def thank_you(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
