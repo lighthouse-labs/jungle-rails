691ae1d68e8ab206333b56d6b2be2bf11d5f36e2
cecstar <yocecia@gmail.com>
2016-11-11 05:25:15 +0000

    added signup page w/ firstname, lastname, email, password. login and logout buttons with routes, both working properly. Has not been styled.

---
+++ db/migrate/20161111023051_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
+  end
+end
