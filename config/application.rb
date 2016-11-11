22790e8fa73d2b1022ce0f180944f21b6d78a82d
Patrick Simonian <patricksimonian@gmail.com>
2016-11-11 11:43:51 -0800

    added html basic authentication for admin page, see your example.env

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# upload .env file for admin authentication
 module Jungle
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
