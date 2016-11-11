d596b24b59f809de3ef1c49aaa99c6a0eb63c773
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-11-11 01:15:11 +0000

    added user signup / login and bcrypt authentification

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
