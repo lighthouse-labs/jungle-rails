a31013e5914ccff77a04f7e0c494c86a188d0919
Patrick Simonian <patricksimonian@gmail.com>
2016-11-10 20:17:28 -0800

    created an email test case for sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "no-reply@jungle.com"
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://localhost:3000'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence:true
-  validates :password, length: {minimum: 8}
+  validates :email, presence:true, uniqueness: true
+  validates :password, length: {minimum: 1}
 
 end
 
