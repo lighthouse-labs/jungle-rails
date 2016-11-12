3d80989a5b54b921882a6f20d13072e8a62d1ced
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-11-12 01:23:13 +0000

    Send formatted email with order items

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
 
-  def order_email(order)
-    puts "Got to user_mailer"
+  def order_receipt(order)
     # @user = user
     @order = order
     # @url = "http://localhost:3000/orders"
-    mail(to: @order.email, subject: @order.id)
+    mail(to: @order.email, subject: "Your Jungle Order ({@order.id})")
   end
 end
