aeeebba07a3c193702d9b7f2b57a90205c0d9a82
cecstar <yocecia@gmail.com>
2016-11-12 01:41:09 +0000

    email receipt now includes interpolated fields for customers product info, quantity, item totals, total sale, and unique items purchased. Renders in the terminal.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def email_receipt(order)
     @order = order
     @url  = 'localhost:3000'
-    mail(to: @order.email, subject: 'Jungle Confirmation: Order #{@order.id}')
+    mail(to: @order.email, subject: 'Jungle Order Receipt: #{@order.id}')
   end
 end
