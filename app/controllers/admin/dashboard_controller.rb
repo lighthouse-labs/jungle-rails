ed4ac093d3f23d30800e1ee75f982c9a0c028043
Nat York <natyork@gmail.com>
2016-11-12 18:21:43 +0000

    Implement feature admin basic authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_UN'], password: ENV['ADMIN_PW']
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_UN'], password: ENV['ADMIN_PW']
+
 
   def index
     @products = Product.order(id: :desc).all
