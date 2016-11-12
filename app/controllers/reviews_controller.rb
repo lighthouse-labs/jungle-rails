3c7b59f429c46c3a2b28966d9502a8266d7fe042
Patrick Simonian <patricksimonian@gmail.com>
2016-11-11 18:04:55 -0800

    form for creating a comment now succusfully posts to the correct product with correct credentials and redirects back to product/show for that product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,5 +2,23 @@ class ReviewsController < ApplicationController
 
   def create
     puts "SUCCESS"
+    @product = Product.find(params.require(:product_id))
+    @review = @product.reviews.new(review_params)
+    # @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to @product
+    else
+      render @review
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
   end
 end
