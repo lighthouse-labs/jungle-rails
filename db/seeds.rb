756223df27de9ad91c755d59fd4b205923a7b738
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-12 01:34:54 +0000

    Add sold out badge to activate when quanity is zero
    Created a sold out badge which comprised of an aside in the html and some modification to the stylesheet.
    An if statement was write in eruby to make sure that the badge only appears when the quanity of an item hits zero.

--- db/seeds.rb
+++ db/seeds.rb
@@ -125,7 +125,7 @@ cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture3.jpg'),
-  quantity: 23,
+  quantity: 0,
   price: 2_483.75
 })
 
