ab6a8bed26a681a43b29f2adaf9080e4429537da
Patrick Simonian <patricksimonian@gmail.com>
2016-11-12 10:16:47 -0800

    added some backend validation for a user to be logged in the review controller so that only logged in users may review a product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,16 +3,20 @@ class ReviewsController < ApplicationController
   def create
     puts "SUCCESS"
     @product = Product.find(params.require(:product_id))
-    @review = @product.reviews.new(review_params)
-    # @review = Review.new(review_params)
+    if current_user
+      @review = @product.reviews.new(review_params)
 
-    # as of now there is no filter for a signed in user
-    @review.user_id = current_user.id
-    if @review.save
-      redirect_to @product
+      # as of now there is no filter for a signed in user
+      @review.user_id = current_user.id
+      if @review.save
+        redirect_to @product
+      else
+        send_error(@review.errors.full_messages)
+        # flash[:send_error] = @review.errors.full_messages
+        # redirect_to @product
+      end
     else
-      flash[:error] = @review.errors.full_messages
-      redirect_to @product
+      send_error(["Must be logged in!"])
     end
   end
 
@@ -21,7 +25,13 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(
       :rating,
-      :description
+      :description,
+      :user_id
     )
+  end
+
+  def send_error(msg)
+    flash[:comment_error] = msg
+    redirect_to @product
   end
 end
