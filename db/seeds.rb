216c2a6b012d9cd3b39bee6564572cfc93281222
Linley Faulkner <linleyjfaulkner@gmail.com>
2016-11-12 00:58:22 +0000

    add logic to sold out badge

--- db/seeds.rb
+++ db/seeds.rb
@@ -117,7 +117,7 @@ cat3.products.create!({
   name:  'Electric Chair',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture2.jpg'),
-  quantity: 2,
+  quantity: 0,
   price: 987.65
 })
 
@@ -125,7 +125,7 @@ cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture3.jpg'),
-  quantity: 23,
+  quantity: 0,
   price: 2_483.75
 })
 
