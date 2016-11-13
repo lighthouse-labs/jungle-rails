766b8260004c48f1415808366d4e6800af0ef07d
Patrick Simonian <patricksimonian@gmail.com>
2016-11-12 22:38:46 -0800

    fixed bug where product review form would not toggle without refreshing the page first

--- app/assets/javascripts/products/products.js
+++ app/assets/javascripts/products/products.js
@@ -1,7 +1,8 @@
-$(document).ready( function () {
-  //toggle review form field for /products/:id
+
+var ready = function() {  //toggle review form field for /products/:id
   $('#review-form-toggle').on('click', function(event) {
     event.stopPropagation();
     $("#product-review").slideToggle();
-  });
-});
+  })
+}
+$(document).on('turbolinks:load', ready);
