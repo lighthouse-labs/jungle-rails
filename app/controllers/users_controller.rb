c151c185741fb7f8f4f62c4914355ba1e5238acb
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-11-15 22:13:09 +0000

    Actually fixed the bug in commit  55884ae3a1a1f222ddb1b7f28b6ebe76ae7cc2c9 (forgot to add the @ symbol in front of user.id)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
        if @user.save
-          session[:user_id] = user.id
+          session[:user_id] = @user.id
           redirect_to '/'
        else
         flash[:error] = @user.errors.full_messages
