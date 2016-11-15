f364c56820693ca091c55b6b491aba9c2296d24b
cecstar <yocecia@gmail.com>
2016-11-15 02:27:39 +0000

    added seed data for Users (3), and refactored existing Review seeds to be more dynamic. Reviews now show user's firstname.

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,9 @@ ActiveRecord::Schema.define(version: 20161112235004) do
     t.datetime "updated_at",  null: false
   end
 
+  add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
     t.string   "lastname"
@@ -77,4 +80,6 @@ ActiveRecord::Schema.define(version: 20161112235004) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
