f7e5b6533bfd4f634d73f6b2f0e68d4aa3caae56
Lucia416 <lucia.awang416@gmail.com>
2017-03-15 23:36:16 +0000

    categories exercises completed

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,7 +10,6 @@ class Admin::CategoriesController < ApplicationController
 
     def create
       @category = Category.new(category_params)
-
       if @category.save
         redirect_to [:admin, :categories], notice: 'Category created!'
       else
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [:show, :index, :new]
 
   resource :cart, only: [:show] do
     put    :add_item
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new]
+    resources :categories, only: [:index, :new, :show, :create]
   end
 
 
