3fb476b431ca9b33394ec50c5816233d92a7593c
Lucia416 <lucia.awang416@gmail.com>
2017-03-17 03:53:38 +0000

    corrected the admin exercise and finished the empty cart, now if cart is empty, redirect with a button

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with username: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['NAME'], password: ENV['PASSWORD']
   def index
     @products = Product.order(id: :desc).all
   end
