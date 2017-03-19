bf59342415794d9fa2d94ad3d5b5bf3cebc8267f
Frosty21 <nathan.froese21@gmail.com>
2017-03-18 19:52:00 -0700

    added user authentication functionality

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  before_filter :authorize
+
   def index
     @products = Product.order(id: :desc).all
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+def current_user
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+end
+
   private
 
   def cart
