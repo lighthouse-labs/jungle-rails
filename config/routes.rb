2523e2d9e7c5a4fb971ed3b5c32b69a3842e7474
macky <mackybeltran1@gmail.com>
2017-03-22 00:21:23 +0000

    adding login, logout, and registration feature

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,14 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
