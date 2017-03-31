2c78ed15b375b6deb890462c614f7f3ff2e00d8a
macky <mackybeltran1@gmail.com>
2017-03-31 16:15:20 +0000

    added empty cart not show the card but a link back to home

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+   http_basic_authenticate_with name: ENV['BANAME'], password: ENV['BAPASS']
 
   def index
     @products = Product.order(id: :desc).all
