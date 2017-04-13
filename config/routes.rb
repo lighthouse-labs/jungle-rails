721d80d94bfb389203037e182acb3bb6dd4c211e
Leanne <leanneherrndorf@gmail.com>
2017-04-13 16:48:51 +0000

    Admin can add categories implemented

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
