181c9bf2e780d2e6d5fad819bfcff54a11596595
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-13 23:17:32 +0000

    sends order receipt to user's email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
 
-  def order_receipt_email(user)
-    @user = user
-    main(to: @user.email, subject: "Jungle Order #{@order.id} Summary")
+  def order_receipt_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Jungle order #{@order.id} receipt")
   end
 end
