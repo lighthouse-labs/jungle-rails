1d3a837cc02a423148d0a55c8480e323efccbb33
Dace Hunter <dacehunter@gmail.com>
2017-04-13 16:20:08 +0000

    allows admin to create new categories

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:new, :index, :create]
-
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
