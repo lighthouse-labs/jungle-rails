477987c31edf556186866d8fdcf8ff6c7e1ef0c3
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-13 19:11:29 +0000

    corrects user def

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
 
   # to receive the form and create a user with form params
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
+    if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -15,8 +15,8 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
-    end
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
