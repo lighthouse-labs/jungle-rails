2d33c6c5fd050ea2c89817892d921dc05c68dc39
Couragyn Chretien <couragyn@gmail.com>
2017-04-13 20:31:17 +0000

    added model and controllers for user registrations and login. Uses session ID

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,13 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
