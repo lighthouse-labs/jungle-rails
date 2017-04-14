943d431e19f9692598a6f7fd7abdb704cebea6a4
Couragyn Chretien <couragyn@gmail.com>
2017-04-14 19:02:54 +0000

    created modle for review. migrated. added validation

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :product
+
+  validates :product, presence: true
+  validates :user, presence: true
+  validates :rating, presence: true
+
+end
