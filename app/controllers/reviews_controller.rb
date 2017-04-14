9d780739529604507ffea74b6344f848a5d0b343
Couragyn Chretien <couragyn@gmail.com>
2017-04-14 23:24:08 +0000

    only logged in users can see the leave a review form. also uses a before_filter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,11 @@
 class ReviewsController < ApplicationController
+
+  before_filter :authorize
+
+  def authorize
+    redirect_to '/' unless current_user
+  end
+
   def create
     @review_params = params[:review]
     review = Review.new(product_id: params[:product_id], user_id: session[:user_id], description: @review_params["description"], rating: @review_params["rating"])
