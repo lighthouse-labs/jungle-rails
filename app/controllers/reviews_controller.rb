2d6caa2bb4a8490cc8be3e82f7bb2f1be584ad7e
Couragyn Chretien <couragyn@gmail.com>
2017-04-14 22:50:40 +0000

    new review form now submits to Database

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,9 @@
 class ReviewsController < ApplicationController
   def create
-    @review.save
-    redirect_to :back
+    @review_params = params[:review]
+    review = Review.new(product_id: params[:product_id], user_id: session[:user_id], description: @review_params["description"], rating: @review_params["rating"])
+    if review.save
+      redirect_to :back
+    end
   end
-
 end
