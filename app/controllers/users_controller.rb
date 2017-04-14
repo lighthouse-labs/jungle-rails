932633e1985c87441467b0ea6fe0df062ef74a10
Michael Mansourati <mmansourati@gmail.com>
2017-04-14 23:03:01 +0000

    finally got register page to render

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:root], notice: "Account created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+end
