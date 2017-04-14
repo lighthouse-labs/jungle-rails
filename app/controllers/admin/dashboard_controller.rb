3fc0c146782eb0b2e1db099f2164fef5cfc5b36f
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-14 00:27:19 +0000

    adds basic HTTP auth for admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  # TODO: move the authentication somewhere else to DRY it up
+  http_basic_authenticate_with name: ENV['ADMIN_BASIC_AUTH_USERNAME'], password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
+
   def show
   end
 end
