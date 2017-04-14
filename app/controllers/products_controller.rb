9e86c766d8074853be485a40e7345d42f9b72a70
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-14 22:12:10 +0000

    passes new review to show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    @reviews = Review.where(product_id: @product.id)
+    @review = Review.new
   end
 
 end
