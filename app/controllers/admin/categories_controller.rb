3706134e81edc3ecc67f1b80d2901ca27eccb0f9
Dace Hunter <dacehunter@gmail.com>
2017-04-14 00:41:47 +0000

    removes hardcoded user/pass and moves to .env

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def index
     @categories = Category.order(:name).all
