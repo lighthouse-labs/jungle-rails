e886505fdf64eebb6c991f4f845a894dee016fda
sadooghi <gh.sadooghi@gmail.com>
2017-04-14 20:56:26 +0000

    added validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
+  validates_associated :product
+  validate :user, presence: true
+  validate :description, presence: true
+  validate :rating, presence: true
+
 end
