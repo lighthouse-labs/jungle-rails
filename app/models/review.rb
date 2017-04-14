7337fb96e56be79ee1a0dd9592720e928190a108
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-14 18:57:51 +0000

    adds validations to model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,10 @@
 class Review < ActiveRecord::Base
 
   belongs_to :product
-  
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
+
 end
