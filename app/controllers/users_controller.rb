54e96fb6bd5603792ec2ee7441f6bfa5616ab1f8
Michael Mansourati <mmansourati@gmail.com>
2017-04-15 19:46:45 +0000

    added user and sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to [:root], notice: "Account created!"
     else
       render :new
@@ -17,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
