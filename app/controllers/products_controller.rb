78065f8e43e8c958cbf9a63f95b59b8cf3e22c9f
sadooghi <gh.sadooghi@gmail.com>
2017-04-15 20:06:02 +0000

    finished exept css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,9 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
+
   validates_associated :product
-  validates :user, presence: true
+  validates :user_id, presence: true
   validates :description, presence: true
   validates :rating, presence: true
 
