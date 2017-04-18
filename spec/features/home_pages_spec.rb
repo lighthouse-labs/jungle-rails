2af2d5b7db41e1cd09157651e63d66b59d0953e3
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-18 20:52:15 +0000

    fixes broken image link in test

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -10,6 +10,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
@@ -21,7 +22,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     visit root_path
 
     # DEBUG
-    # save_screenshot
+    save_screenshot
 
     # VERIFY
     expect(page).to have_css 'article.product', count: 10
