4896f7ac2b06467cc784a94630468fb1076e860f
sadooghi <gh.sadooghi@gmail.com>
2017-04-18 00:19:33 +0000

    change prod mailer to not raise errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -134,7 +134,7 @@ cat3.products.create!({
 
 # reviews
 
-u = User.create({first_name: Faker::Name.first_name ,last_name: Faker::Name.last_name , password_digest:Faker::Name.last_name , email: Faker::Internet.email})
+u = User.create({first_name: Faker::Name.first_name ,last_name: Faker::Name.last_name , password: 'password', password_confirmation: 'password', email: Faker::Internet.email})
 
 Review.destroy_all
 
