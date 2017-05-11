4b26e55c39a1bd3c8aa8106d453cc1e9e53b19f7
Jinny Huang <jinny921@gmail.com>
2017-05-11 20:37:52 +0000

    completed user register and login authentication

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  resources :users, only: [:create, :new]
+  resource :session, only: [:create, :new, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
