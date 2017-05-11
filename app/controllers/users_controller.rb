db18a996b6efe52401791658f3b16b7b52bf512b
Jinny Huang <jinny921@gmail.com>
2017-05-11 20:46:42 +0000

    small change on indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
   
 
   def create
-    user = User.new(user_params)
-
     if user.save
       session[:user_id] = user.id
       redirect_to '/', notice: 'User created!'
