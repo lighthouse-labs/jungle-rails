8279f52cadf29b9c4b384a51fcede7f6a733e60d
Arjun <lallarjun@gmail.com>
2017-06-12 22:16:17 +0000

    testing user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,24 @@
 class User < ActiveRecord::Base
   has_many :reviews
+  validates :email, :uniqueness => {:case_sensitive => false}
   # gives our User model authentication methods via bcrypt.
+  validates :password, presence: true, length: {minimum: 6}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password_confirmation, presence: true
   has_secure_password
+  before_save :downcase_strip_email
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase!)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def downcase_strip_email
+    self.email.downcase!
+  end
 end
