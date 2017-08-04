986ebdd5d3bc284231794a9ef4c3ddf8071a263b
Denny Hollick <dennyhollick@gmail.com>
2017-08-03 21:00:57 -0700

    Added create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 
 end
