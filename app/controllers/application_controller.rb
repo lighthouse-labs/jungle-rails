d924691571392e14cfb1c91ee95afd6c481340a0
Denny Hollick <dennyhollick@gmail.com>
2017-08-03 21:00:41 -0700

    Added def to handle sessions/login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,14 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
+
 end
