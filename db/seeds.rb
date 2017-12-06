18fbe22799245c1126bf806131bb77052356c67d
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-12-06 22:13:13 +0000

    sold out tag applied

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,7 +71,7 @@ cat1.products.create!({
   name:  'Russian Spy Shoes',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel5.jpg'),
-  quantity: 8,
+  quantity: 0,
   price: 1_225.00
 })
 
