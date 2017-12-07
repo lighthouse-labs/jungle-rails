7c39ec646f5a50675d82c3a3db05726921f3a498
montygoldy <montygoldy2006@gmail.com>
2017-12-07 19:29:25 +0000

    Added create and show categories functionality to the admin panel

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
 
   has_many :products
-
+  validates :name, presence: true,
+                    length: { minimum: 3 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, :categories, except: [:edit, :update, :show]
-
+    resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
