8ee2c2dbb46b465d0813f1670773030e284735b8
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-07 23:21:29 +0000

    Created new controller/route/view, copying templates from products

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
+
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
