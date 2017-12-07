52e34422e44a094dcc9438ac91b55ab80d4488f1
reidnaa <reidnaaykens@gmail.com>
2017-12-07 21:07:10 +0000

    categories work now..

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
   def index
-    @categories = Category.order(id: :desc).all
+    @category = Category.order(id: :desc).all
   end
 
   def new
@@ -12,17 +12,24 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'category created!'
     else
       render :new
     end
   end
 
+
   private
 
   def category_params
     params.require(:category).permit(
-      :name
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
     )
   end
+
 end
