1b8ef8759ab98fd0707532deb96db01eefc1f9a1
montygoldy <montygoldy2006@gmail.com>
2017-12-07 23:41:05 +0000

    Removed authorization from products and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-before_action :authorize
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
