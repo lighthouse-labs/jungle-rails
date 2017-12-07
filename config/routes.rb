f9b9d07419d86263e76a3bcdf510d2421853a34e
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-07 23:37:59 +0000

    Following https://gist.github.com/thebucknerlife/10090014 -- step 3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,12 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show]
-
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
+
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
