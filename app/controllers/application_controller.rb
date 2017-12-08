57f561df76a7d89825b4a2906e9e7df106b76875
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-08 02:02:07 +0000

    Fixed conditional login/logout link dislay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
+
+    helper_method :current_user
 
   def authorize
     redirect_to '/login' unless current_user
