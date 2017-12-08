54e6d42dd59fa4d5188b49bbaf891d1acc0595f9
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-08 23:21:12 +0000

    Found and inserted form code, still needs pretty

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @review = @product.reviews.new
   end
 
 end
--- app/views/reviews/_review.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! review, :id, :product_id, :user_id, :description, :rating, :created_at, :updated_at
-json.url review_url(review, format: :json)
