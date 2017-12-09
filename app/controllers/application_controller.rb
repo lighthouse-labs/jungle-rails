f28517ca865b17f52e96dfe353694b1a04047994
montygoldy <montygoldy2006@gmail.com>
2017-12-09 21:25:25 +0000

    Added review and rating stystem with delete functionality for the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :current_user
+   before_action :current_user
 
   def current_user
-    current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def authorize
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
 
+  before_action :current_user
 
   def create
 
