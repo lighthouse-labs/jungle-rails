d129bc2b943f4050f7557c88b7346550b0b2e054
montygoldy <montygoldy2006@gmail.com>
2017-12-09 00:41:59 +0000

    fixed few bugs related reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def authorize
