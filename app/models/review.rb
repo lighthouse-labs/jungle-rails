2ae921196e242a84b96525cae44fc17aeb556eb7
V. Anastassiou <v.anastassiou@gmail.com>
2017-12-09 00:33:13 +0000

    Can retrieve list of all reviews for product.

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,7 @@ class Review < ActiveRecord::Base
 
   validates :description, presence: true
   validates :rating, presence: true
+  validates :product_id, presence: true
+  validates :user_id, presence: true
 
 end
