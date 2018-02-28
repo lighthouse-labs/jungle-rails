4abf0559a8d2aec45caaaf7c77a92bc991a3b35f
Zushisan <74838@supinfo.com>
2018-02-28 00:55:22 +0000

    added a category management feature

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,35 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  # def destroy
+  #   @product = Product.find params[:id]
+  #   @product.destroy
+  #   redirect_to [:admin, :products], notice: 'Product deleted!'
+  # end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
