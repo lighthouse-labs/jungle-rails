f2b1ec22422c2b39772e5c3fc5bd19b7a058fe02
Zushisan <74838@supinfo.com>
2018-02-28 21:40:31 +0000

    Finalized user register/login/logout feature available from any page thanks to the nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
     def new
+      @user = User.new
     end
 
     def create
       user = User.new(user_params)
       if user.save
         session[:user_id] = user.id
-        redirect_to [:root], notice: 'Login Successful'
+        redirect_to [:root], notice: 'Registration Successful'
       else
-        render :new
+        render :new, notice: 'Registration Failed !'
       end
     end 
     
