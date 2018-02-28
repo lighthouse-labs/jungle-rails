5f18215fa60b69fc928d87ad39194706796dc9d9
Taylor <thulsmans.2994@me.com>
2018-02-28 00:32:31 +0000

    adds index, new, create to admin::categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,23 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories =  Category.order(id: :desc).all
   end
   def new
+    @category = Category.new
   end
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to [:admin, :products], notice: 'Category Created!'
+    else
+      render :new
+    end
   end
+
+
+  private
+  def category_params
+    params.require(:cateogry).permit(
+      :name
+      )
 end
