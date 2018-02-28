6df52139baa8082fd2f6c35b58562ce857108f0d
tjbeirao <tjbeirao@gmail.com>
2018-02-28 15:59:28 -0500

    initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  # get "/*anything" to 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
