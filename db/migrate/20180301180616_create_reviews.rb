715b7d892512a85c8ee575b1353e420cf3a30f39
Zushisan <74838@supinfo.com>
2018-03-01 19:26:19 +0000

    added the review table and seeded some default reviews

--- db/migrate/20180301180616_create_reviews.rb
+++ db/migrate/20180301180616_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.references :product, index: true, foreign_key: true
-      t.references :user, index: true, foreign_key: true
+      t.integer :user_id
       t.string :description
       t.integer :rating
 
