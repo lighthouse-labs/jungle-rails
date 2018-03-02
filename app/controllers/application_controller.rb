0f88c298517cf2c61dbd42fcbdeeadb0f5ca3397
Taylor <thulsmans.2994@me.com>
2018-03-02 04:54:25 +0000

    repaired visitor permissions, only users can write reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
   #def current_user
    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
