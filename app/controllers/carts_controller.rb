dc6ddb0d07e60a40e312c32cbf827b5b68dd88fb
tjbeirao <tjbeirao@gmail.com>
2018-03-02 12:38:27 -0500

    notification if you cart is empty applyed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
 
   def show
+    @cart = cart
   end
 
   def add_item
@@ -25,5 +26,4 @@ class CartsController < ApplicationController
 
     redirect_to :back
   end
-
 end
