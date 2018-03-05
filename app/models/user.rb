b165c57b9773e7da71dc82e05a2efb5c489a6104
Taylor <thulsmans.2994@me.com>
2018-03-05 23:01:51 +0000

    added some tests for custom devise properties

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   # has_secure_password breaks devise, which has better auth
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe User, type: :model do
       user = User.create(first_name: 'Taylor', last_name: 'Hulsmans', email:'thulsmans.2994@me.com', password:'topsecret')
       expect(user).to be_valid
     end
+
+    it 'a devise user without a first_name is rejected' do
+      user = User.create(first_name: nil, last_name: 'Hulsmans', email:'thulsmans.2994@me.com', password:'topsecret')
+      expect(user).to_not be_valid
+    end
+
+    it 'a devise user without a last_name is rejected' do
+      user = User.create(first_name: 'Taylor', last_name: nil, email:'thulsmans.2994@me.com', password:'topsecret')
+      expect(user).to_not be_valid
+    end
   end
 end
