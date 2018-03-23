eb255f25828ad9263e2922fc9f7e12a1b5705031
tjbeirao <tjbeirao@gmail.com>
2018-03-23 23:43:24 +0000

    update of the images

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
-  
+
   def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
+      # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to [:root], notice: 'Login Successful'
