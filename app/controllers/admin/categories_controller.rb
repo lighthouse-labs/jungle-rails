1231a050d7e1d3b9a31265491d38514053d9c343
Andy <voandy1998@gmail.com>
2018-04-27 01:30:22 +0000

    added admin authentication

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['ADMIN'], :password => ENV['MY_PASS']
 
   def index
     @categories = Category.order(id: :desc).all
