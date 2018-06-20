f97239b4538e41d09b88c9f97e9d68ed37d9604a
jeffvtang <jeff.tang@gmail.com>
2018-06-20 21:56:01 +0000

    completed add category form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,11 +4,26 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
-    
+    @category = Category.new
   end
 
   def create
-    
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category added!'
+    else
+      render :new
+    end
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
 
 end
