0badfd4eeefdecae29f15d43d1efbcf30c7dc8d4
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-06-20 15:17:07 +0000

    Feature: Product Sold Out - Badge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.web_console.whitelisted_ips = ['10.0.2.0/24']
+  # 
+  # config.reload_classes_only_on_change = true
+  # config.file_watcher = true
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
