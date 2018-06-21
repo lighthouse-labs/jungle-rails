02694015bc061dd7000fca7dba1a2e03081b264e
jeffvtang <jeff.tang@gmail.com>
2018-06-21 21:30:10 +0000

    completed user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to '/'
     else
       redirect_to '/login'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user_id
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
       redirect_to '/'
     else
-      render 'new'
+      redirect_to '/signup'
     end
-  end 
+  end
   
   private
 
