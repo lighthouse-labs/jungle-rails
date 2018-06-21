83b2ec79848e906c7f1fa391a1e7967fe564a015
jeffvtang <jeff.tang@gmail.com>
2018-06-21 20:41:00 +0000

    added logic to userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
-  end   
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.:user_id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end 
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
