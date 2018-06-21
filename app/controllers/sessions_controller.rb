00a3df6f047c0e18aeef85a81dc5564f964c577a
jeffvtang <jeff.tang@gmail.com>
2018-06-21 20:54:15 +0000

    added sessioncontroller logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.user_id
+      redirect_to '/'
+    else
+      redirect_to '/login'
   end
+end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.:user_id
+      session[:user_id] = user.user_id
       redirect_to '/'
     else
       redirect_to '/signup'
