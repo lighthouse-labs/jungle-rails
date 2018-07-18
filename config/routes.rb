05aee88cad72d5e37221ecddb8a451a51dfc8cb3
leor8 <leo442183205@gmail.com>
2018-07-18 00:45:00 +0000

    admin now can add categories to the list

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,13 @@ Rails.application.routes.draw do
     delete :remove_item
   end
 
+
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
