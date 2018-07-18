e3a9dab4f97366bded33f183a3751815ad77f818
Jeremy Paterson <jaydpaterson@gmail.com>
2018-07-18 01:13:32 +0000

    Basic auth working for admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASS']
   def index
     @categories = Category.order(id: :desc).all
   end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASS']
   def show
+    render plain: "Everyone can see me: "
+  end
+
+  def others
+    render plain: 'Can\'t see me though'
   end
 end
