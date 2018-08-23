7891e66ff566d8d3665481e63085b91a3c9e321b
Khurram Virani <kvirani@gmail.com>
2018-08-23 13:47:21 -0700

    fix a bit too much vertical spacing in controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,7 +5,6 @@ class CartsController < ApplicationController
 
   def add_item
     product_id = params[:product_id].to_s
-
     modify_cart_delta(product_id, +1)
 
     redirect_to :back
@@ -13,7 +12,6 @@ class CartsController < ApplicationController
 
   def remove_item
     product_id = params[:product_id].to_s
-
     modify_cart_delta(product_id, -1)
 
     redirect_to :back
