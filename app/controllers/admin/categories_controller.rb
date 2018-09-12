50aa0f046f9931580aaa71d9789ed5bfc8c0f8a8
Julia Zhou <juliazhouism@gmail.com>
2018-09-12 22:35:45 +0000

    categories views created

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,19 +1,26 @@
 class Admin::CategoriesController < ApplicationController
     
     def index
-        @categories = Category.order(id: :desc).all
-      end
-    
-      def new
+        @categories = Category.order(:name).all
+    end
+
+    def new
         @category = Category.new
-      end
-    
-      def create
+    end
+
+    def create
         @category = Category.new(product_params)
-    
+
         if @category.save
-          redirect_to [:admin, :categories], notice: 'Category created!'
+            redirect_to [:admin, :categories], notice: 'Category created!'
         else
-          render :new
+            render :new
         end
-      end
+    end
+
+    private
+
+    def category_params
+    params.require(:category).permit(:name)
+    end
+end
