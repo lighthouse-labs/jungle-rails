ccbb9f740f14649d95c5c7cc472659f0e5fa64a2
James Dowell <james.s.dowell@gmail.com>
2018-09-12 21:39:04 +0000

    added sessions. Added contextual links in nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+  validates :email, :uniqueness => {:case_sensitive => false}
 end
