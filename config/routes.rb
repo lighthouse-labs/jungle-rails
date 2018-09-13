f023c44d798e4deec2a2d2c4c9fe6e7debc1f8b9
Guy <guy.mcauliffe@icloud.com>
2018-09-13 03:29:06 +0000

    Gemfile.lock updated by installation

--- config/routes.rb
+++ config/routes.rb
@@ -72,3 +72,20 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
+# Routes
+#            Prefix Verb   URI Pattern                   Controller#Action
+#              root GET    /                             products#index
+#          products GET    /products(.:format)           products#index
+#           product GET    /products/:id(.:format)       products#show
+#          category GET    /categories/:id(.:format)     categories#show
+#     add_item_cart POST   /cart/add_item(.:format)      carts#add_item
+#  remove_item_cart POST   /cart/remove_item(.:format)   carts#remove_item
+#              cart GET    /cart(.:format)               carts#show
+#            orders POST   /orders(.:format)             orders#create
+#             order GET    /orders/:id(.:format)         orders#show
+#        admin_root GET    /admin(.:format)              admin/dashboard#show
+#    admin_products GET    /admin/products(.:format)     admin/products#index
+#                   POST   /admin/products(.:format)     admin/products#create
+# new_admin_product GET    /admin/products/new(.:format) admin/products#new
+#     admin_product DELETE /admin/products/:id(.:format) admin/products#destroy
