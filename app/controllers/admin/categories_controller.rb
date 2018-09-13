1de0e86f220ac897d734c1048a0a3eae87684d59
Julia Zhou <juliazhouism@gmail.com>
2018-09-13 01:04:56 +0000

    route for categories fixed

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,7 +9,7 @@ class Admin::CategoriesController < ApplicationController
     end
 
     def create
-        @category = Category.new(product_params)
+        @category = Category.new(category_params)
 
         if @category.save
             redirect_to [:admin, :categories], notice: 'Category created!'
