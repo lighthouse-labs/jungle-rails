9f6087b162b954c4e94a4e436bce2aa9d749cd1b
James Dowell <james.s.dowell@gmail.com>
2018-09-13 03:35:46 +0000

    added user mailer view file

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: "no-reply@jungle.com"
-
+  default from: 'notifications@example.com'
+ 
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
