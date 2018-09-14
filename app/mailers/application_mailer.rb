8762fde52f21943ca2e1aadab9ce4f0338ea6534
Julia Zhou <juliazhouism@gmail.com>
2018-09-14 00:38:41 +0000

    send order confirmation email contains product info

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@jungle.com"
   layout 'mailer'
-
-  # send a order confirmation email to the user, pass in the user object that contains the user's email address
-  def send_order_email(user)
-    @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for signing up for shopping at Jungle' )
-  end
-
 end
