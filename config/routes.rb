1f5ea4bc8720bd08c56e476108634723d9a06d46
James Dowell <james.s.dowell@gmail.com>
2018-09-14 01:09:09 +0000

    added delete button

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
   end
   resources :categories, only: [:show]
+  resources :reviews, only: [:destroy]
 
   resource :cart, only: [:show] do
     post   :add_item
