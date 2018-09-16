649cbbe3941e16b255e28974476d168de98efd62
Guy <guy.mcauliffe@icloud.com>
2018-09-16 05:48:25 +0000

    Review form posting successfully and rendering

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,27 @@
 class ReviewsController < ApplicationController
+  
+  before_action :require_login
+  
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to @product
+    else
+      render 'products/show'
+    end
   end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:description, :rating)
+    end
+
+    def require_login
+      redirect_to path_to_product unless current_user
+    end
+
 end
