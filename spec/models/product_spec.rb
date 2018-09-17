02d498565001ca23d66aedce8740b6506b6bea89
James Dowell <james.s.dowell@gmail.com>
2018-09-17 18:59:27 +0000

    added new product validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  before(:each) do
+    @category = Category.new name:'Apparel'
+    @product = Product.new
+  end
+
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      @product_params = Product.new(name:'name', description: 'description', category:@category, quantity:5, image:'url', price:100)
+      expect(@product_params).to be_valid
+    end
+
+    it 'is not valid without a name' do 
+      expect(@product).to_not be_valid
+    end
+    it 'is not valid without a price' do 
+      expect(@product).to_not be_valid
+    end
+    it 'is not valid without a quantity' do 
+      expect(@product).to_not be_valid
+    end
+    it 'is not valid without a category' do 
+      expect(@product).to_not be_valid
+    end
+  end
+end
