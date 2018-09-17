55ed2d054115ec87875ca9539db651a064a5bc04
James Dowell <james.s.dowell@gmail.com>
2018-09-17 19:03:14 +0000

    refined product tests to include other parameters

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Product, type: :model do
 
   before(:each) do
     @category = Category.new name:'Apparel'
-    @product = Product.new
   end
 
   describe 'Validations' do
@@ -14,15 +13,19 @@ RSpec.describe Product, type: :model do
     end
 
     it 'is not valid without a name' do 
+      @product = Product.new(category:@category, quantity:5, price:100)
       expect(@product).to_not be_valid
     end
     it 'is not valid without a price' do 
+      @product = Product.new( name:'name', category:@category, quantity:5)
       expect(@product).to_not be_valid
     end
     it 'is not valid without a quantity' do 
+      @product = Product.new(name:'name', category:@category, price:100)
       expect(@product).to_not be_valid
     end
     it 'is not valid without a category' do 
+      @product = Product.new(name:'name', quantity:5, price:100)
       expect(@product).to_not be_valid
     end
   end
