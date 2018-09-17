46343cd2a2773fa89d2ec54dd8a02ef0ac1cbc68
James Dowell <james.s.dowell@gmail.com>
2018-09-17 19:08:35 +0000

    added error message validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,22 +11,26 @@ RSpec.describe Product, type: :model do
       @product_params = Product.new(name:'name', category:@category, quantity:5, price:100)
       expect(@product_params).to be_valid
     end
-
+    
     it 'is not valid without a name' do 
       @product = Product.new(category:@category, quantity:5, price:100)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
     it 'is not valid without a price' do 
       @product = Product.new( name:'name', category:@category, quantity:5)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
     it 'is not valid without a quantity' do 
       @product = Product.new(name:'name', category:@category, price:100)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
     it 'is not valid without a category' do 
       @product = Product.new(name:'name', quantity:5, price:100)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
