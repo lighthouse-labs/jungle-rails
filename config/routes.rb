5b9d1bd963490b88d14f46446e182f26a88f36ba
erik-mackie <erik.mackie@gmail.com>
2018-10-10 22:08:48 +0000

    changed rake

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  
 
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
+
 
   resources :orders, only: [:create, :show]
 
