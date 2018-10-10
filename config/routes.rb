4891b98b440985a6b89a8b679242aac588bc6784
kkwon39 <kkwon39@gmail.com>
2018-10-10 21:34:50 +0000

    finished admin-categories feature

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources  :categories, only: [:index, :new, :create]
+    resources  :products, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
