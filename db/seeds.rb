cb8cda6ad2ba3715c1699727e3405be243b5b1aa
Thofeeq <thofeeqm@gmail.com>
2018-10-10 18:21:01 +0000

    Added sold out badge on the product card

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,7 +63,7 @@ cat1.products.create!({
   name:  'Hipster Socks',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel4.jpg'),
-  quantity: 8,
+  quantity: 0,
   price: 25.00
 })
 
@@ -128,7 +128,7 @@ cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture3.jpg'),
-  quantity: 23,
+  quantity: 0,
   price: 2_483.75
 })
 
