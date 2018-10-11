b1d137ed01f049fc3553f5035b68f182c87a9185
Thofeeq <thofeeqm@gmail.com>
2018-10-11 01:04:16 +0000

    Added a detailed page for post order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < ApplicationController
 
   def create_order(stripe_charge)
     order = Order.new(
-      email: params[:stripeEmail],
+      email: current_user ? current_user.email : "guest",
       total_cents: cart_subtotal_cents,
       stripe_charge_id: stripe_charge.id, # returned by stripe
     )
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
-
+  validates :name, presence: { message: "must be given please" }
+  
   has_many :products
 
 end
