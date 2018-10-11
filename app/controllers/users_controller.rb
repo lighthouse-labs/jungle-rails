8344eff5211ae112cb56d9b2fcedf10739d1c988
erik-mackie <erik.mackie@gmail.com>
2018-10-11 19:36:08 +0000

    user controller, no longer redirects on logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,7 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
-    else
-      redirect_to '/signup'
-    end
+    
   end 
   
   private
