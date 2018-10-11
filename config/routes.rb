57342b0143140fed6cec3ecec3d0f3eb995524d9
erik-mackie <erik.mackie@gmail.com>
2018-10-11 01:03:48 +0000

    routes, constructors, admin index

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :create, :new]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
