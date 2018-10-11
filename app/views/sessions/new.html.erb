5f62d640dd85cac4bab114de22e1429bcfe5aab4
kkwon39 <kkwon39@gmail.com>
2018-10-11 19:20:13 +0000

    completed user-athentication feature

---
+++ app/views/sessions/new.html.erb
@@ -0,0 +1,11 @@
+<!-- app/views/sessions/new.html.erb -->
+
+<h1>Login</h1>
+
+<%= form_tag '/login' do %>
+
+  Email: <%= text_field_tag :email %>
+  Password: <%= password_field_tag :password %>
+  <%= submit_tag "Submit" %>
+
+<% end %>
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,18 @@ Rails.application.routes.draw do
     resources  :products, except: [:edit, :update, :show]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+end
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -72,4 +84,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
