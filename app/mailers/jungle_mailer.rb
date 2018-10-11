0c2f0ee5858c4e9ebabc79c9564d8ff0471ed7ed
Thofeeq <thofeeqm@gmail.com>
2018-10-11 21:41:43 +0000

    Refactoring the jungle mailer

--- app/mailers/jungle_mailer.rb
+++ app/mailers/jungle_mailer.rb
@@ -1,12 +1,4 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :authentication => :plain,
-    :user_name => "junglerailstest@gmail.com",
-    :password => "H4kunamatata",
-    :enable_starttls_auto => true
-}
+
 class JungleMailer < ApplicationMailer
   default from: 'junglerailstest@gmail.com'
  
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,16 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :user_name => "junglerailstest@gmail.com",
+    :password => "H4kunamatata",
+    :enable_starttls_auto => true
+}
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
