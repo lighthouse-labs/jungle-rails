5121cf14cc87b2e40509f3e48ea2b2b168b5c46c
Thofeeq <thofeeqm@gmail.com>
2018-10-12 19:05:15 +0000

    Added product/user prescence validation to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ActiveRecord::Base
   validates :rating, presence: true
   validates :description, presence: true
+
+  belongs_to :user
+  belongs_to :product
+
+  validates :product, presence: true
+  validates :user, presence: true
 end
