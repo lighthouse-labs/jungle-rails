72634cd5338de86c44ae29620192da002e0515b3
Thofeeq <thofeeqm@gmail.com>
2018-10-12 18:50:12 +0000

    Added validations to Review,Product and User model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,12 @@
 class Product < ActiveRecord::Base
-
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :review, dependent: :destroy
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
-
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ActiveRecord::Base
+  validates :rating, presence: true
+  validates :description, presence: true
 end
