eb2828c2fb77d386966ba2d02343569a0835a47f
erik-mackie <erik.mackie@gmail.com>
2018-10-12 18:32:25 +0000

    reviews generator ran

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,12 @@ class Review < ActiveRecord::Base
 
   belongs_to :product
   
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
+
+  # from products
+  # validates :quantity, presence: true
+
 end
