cb0d6dd1d1af237b10a0885744c9132897c2bfb0
erik-mackie <erik.mackie@gmail.com>
2018-10-13 01:55:45 +0000

    review create from form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,25 @@
 class ReviewsController < ApplicationController
 
-  def create
+  
     
-  end
+
+    def create
+
+      @review = Review.new({
+        product_id: params[:product_id],
+        description: params[:review][:description],
+        rating: params[:review][:rating] 
+      })
+      @review.user = current_user
+
+
+      if @review.save
+        redirect_to "/"
+        
+      else
+        redirect_to @review.product
+      end
+    end
+  
 
 end
