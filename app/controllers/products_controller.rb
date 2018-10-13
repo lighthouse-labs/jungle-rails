442e3821074939c7ad86fa98826452e1d552d3f4
erik-mackie <erik.mackie@gmail.com>
2018-10-13 02:34:57 +0000

    only signed in user can leave reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = @product.reviews.new
+    @current_user = current_user
+    
   end
 
 end
