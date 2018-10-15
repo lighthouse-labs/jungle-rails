142b9e059be8c5a5a5214eaac57dafe0fa2b14bf
kkwon39 <kkwon39@gmail.com>
2018-10-15 20:17:33 +0000

    finished test feature for email length and whitespaces+case sensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+
+  validates :password, length: {minimum: 4}, presence: true
+  validates :email, uniqueness: {:case_sensitive => false}
+  validate :password_matches
+
+  def password_matches
+    if password != password_confirmation
+      errors.add(:password_confirmation, "has to match password")
+    end
+  end
+
+  def self.authenticate_with_credentials(email, password)
+
+    user = User.where('lower(email) = ?', email.downcase.strip).first
+    if user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "should successfully save" do
+      user = User.new
+      user.name = "Kelvin Kwon"
+      user.email = "kkwon39@gmail.com"
+      user.password = "1234"
+      user.password_confirmation = "1234"
+      expect(user).to be_valid
+
+      user2= User.new
+      user.name = "Kelvin Kwon2"
+      user.email = "kkwon39@gmail.com"
+      user.password = "1234"
+      user.password_confirmation = "1234"
+      expect(user2).not_to be_valid
+    end
+
+    it "should not successfully save" do
+      user = User.new
+      user.name = "Kelvin Kwon"
+      user.email = "kkwon39@gmail.com"
+      user.password = "2345"
+      user.password_confirmation = nil
+      expect(user).not_to be_valid
+    end
+
+    it "should not successfully save" do
+      user = User.new
+      user.name = "Kelvin Kwon"
+      user.email = "kkwon39@gmail.com"
+      user.password = "25"
+      user.password_confirmation = "25"
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it "should match only if emails and passwords are same" do
+      user = User.new
+      user.name = "Kelvin Kwon"
+      user.email = "kkwon39@gmail.com"
+      user.password = "2345"
+      user.password_confirmation = "2345"
+      user.save
+
+      user2 = User.authenticate_with_credentials("     kkwon39@gmail.com", "2345")
+      user2.save
+      expect(user.id).to be(user2.id)
+    end
+
+    it "should match only if emails and passwords are same" do
+      user = User.new
+      user.name = "Kelvin Kwon"
+      user.email = "kkWON39@gmail.com"
+      user.password = "2345"
+      user.password_confirmation = "2345"
+      user.save
+
+      user2 = User.authenticate_with_credentials("KkWoN39@gmail.com", "2345")
+      user2.save
+      expect(user.id).to be(user2.id)
+    end
+  end
 end
