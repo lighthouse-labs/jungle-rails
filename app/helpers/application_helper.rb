c7a124640cb125b5fdbd5efcc1dd7004defac037
Laith Azer <laith.azer@gmail.com>
2018-10-15 19:30:43 -0400

    Adds code to display flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level.to_s
+      when 'notice', 'success' then 'alert alert-success'
+      when 'error', 'alert' then 'alert alert-danger'
+    end
+  end
 end
