bfb3df018e2cdda1828d0a003826cb0cdd87783b
kkwon39 <kkwon39@gmail.com>
2018-10-16 17:26:38 +0000

    finished add_to_carts testing

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product details" do
+
+    visit root_path
+    save_screenshot
+    click_on("Add")
+    # commented out b/c it's for debugging only
+    expect(page).to have_text 'My Cart (1)'
+    save_screenshot
+  end
+end
