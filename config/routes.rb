27515fc5f1a6327f93d8b2beddcb2f120892b8f8
Julia <juliaromanowski@gmail.com>
2019-02-20 23:08:59 +0000

    creating categories routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [:index, :new, :create]
 
   resource :cart, only: [:show] do
     post   :add_item
