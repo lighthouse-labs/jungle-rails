9dc9c74e57ea93d10d95de99392f597318258cef
Rachel Wong <rachelwong.ly@gmail.com>
2019-02-25 22:16:37 +0000

    added validations to review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
 end
