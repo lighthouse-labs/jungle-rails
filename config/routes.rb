ef8d2506df935d7a0ccbb44daa0d7f02bd26b762
Rachel Wong <rachelwong.ly@gmail.com>
2019-02-26 18:25:51 +0000

    route for create review added

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   post '/users' => 'users#create'
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only [:create]
+    resources :reviews, only: [:create]
   end
-  
+
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
