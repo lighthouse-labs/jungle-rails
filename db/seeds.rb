1abf52e96dbd63c60b22d42027aaa957e40f5781
Paulyn <paulyntran@live.ca>
2019-03-21 00:07:04 +0000

    Added sold out badge when quantity is 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,7 +96,7 @@ cat2.products.create!({
   name:  'Hotdog Slicer',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('electronics2.jpg'),
-  quantity: 3,
+  quantity: 0,
   price: 26.00
 })
 
