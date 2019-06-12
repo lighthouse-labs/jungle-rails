fb7ccbf7081808676c4df4c16ae2a81e79aed518
ZHShang <zhihengshang@hotmail.com>
2019-06-12 16:10:25 +0000

    linked the register to the correct form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :users
+  resources :users, only: [:index, :new]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
