62deeeb7a0b02713beb1766168822aa462067587
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-13 19:42:00 -0400

    working login signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
 
+  
+  
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+  
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-    has_secure_password
+    has_secure_password 
 end
