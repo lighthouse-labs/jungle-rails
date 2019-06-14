a75a1086cd067b1614d3b4cda7e2b11d09683efc
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-14 18:51:56 -0400

    fixed admin auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
 
   def index
     @products = Product.order(id: :desc).all
