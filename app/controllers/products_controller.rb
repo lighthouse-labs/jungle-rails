ac9577fa3a8484d27ca73c3ce18e2250624fb9c1
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-15 00:46:00 -0400

    seeds working, reviews rendering on product show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @reviews = Review.where(product_id: @product.id)
+    puts @reviews
   end
   
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,15 @@
+class ReviewsController < ApplicationController
+    def index
+    end
+    def show
+    end
+    def create
+    end
+    def destroy
+    end
+    def new
+    end
+    def edit
+    end
+
+end
