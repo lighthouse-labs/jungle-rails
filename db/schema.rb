aedf2674bf2d57b4661a3e3ff567d5e115f71252
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-14 22:46:07 -0400

    added foreign key to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190615011857) do
+ActiveRecord::Schema.define(version: 20190615021139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,11 @@ ActiveRecord::Schema.define(version: 20190615011857) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "review_id"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+  add_index "products", ["review_id"], name: "index_products_on_review_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "product_id"
@@ -78,4 +80,5 @@ ActiveRecord::Schema.define(version: 20190615011857) do
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "products", "reviews"
 end
