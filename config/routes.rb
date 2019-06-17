546b64500014987c43e11cf73a630f59593a5e0b
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-17 17:56:55 -0400

    installed rspec and created test db

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories
+    resources :categories, only: [:index, :new, :create]
   end
 
     
