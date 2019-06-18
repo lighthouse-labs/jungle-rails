85ce709478a1c8008819ed7324d32ebb452838e3
Renkinjutsu <lucas.cheng12@gmail.com>
2019-06-18 11:55:20 -0400

    added user authenticate_with_credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
     end
   
     def create
-        user = User.find_by_email(params[:email])
+      user = User.authenticate_with_credentials(params[:email], params[:password])
         # If the user exists AND the password entered is correct.
-        if user && user.authenticate(params[:password])
+        if user.present?
+
           # Save the user id inside the browser cookie. This is how we keep the user 
           # logged in when they navigate around our website.
           session[:user_id] = user.id
@@ -22,4 +23,6 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         redirect_to '/login'
     end
+
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
     validates_uniqueness_of :email, confirmation: { case_sensitive: false }
     validates :password, length: {minimum: 6}
     has_secure_password 
+
+    def self.authenticate_with_credentials(email, password)
+        user = User.find_by(email: "#{email}")
+        user.authenticate(password)
+    end
 end
