d2722ff457279245086983efd433866206b3c7b0
Perry <perrymacdougall@gmail.com>
2019-07-10 23:17:05 +0000

    rendered list of categories on the front end

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,19 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+end
