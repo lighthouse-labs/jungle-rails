46be0a12be220e6c2830fd26c085aed37fd28e54
Perry <perrymacdougall@gmail.com>
2019-07-10 23:52:21 +0000

    defined category_params to complete creation of new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -16,4 +16,12 @@ class Admin::CategoriesController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 end
