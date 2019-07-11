2d78149db2d9936683758b6a5f94a4381b5a027b
Perry <perrymacdougall@gmail.com>
2019-07-11 20:52:16 +0000

    fixed user params bug

--- db/migrate/20190711160906_create_users.rb
+++ db/migrate/20190711160906_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
+      t.string :last_name
       t.string :email
       t.string :password_digest
 
--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20190711160906) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
