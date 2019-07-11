745bed4aaa24a118cb4a23ef540e93b27a704616
mmythily <mythily.mudunuru@gmail.com>
2019-07-10 21:54:09 -0400

    show category in index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,4 +6,21 @@ class Admin::CategoriesController < ApplicationController
     def new
         @category = Category.new
     end
+
+    def create
+        @category = Category.new(category_params)
+    
+        if @category.save
+            redirect_to [:admin, :categories], notice: 'Category created!'
+        else
+            render :new
+        end
+    end
+    private
+
+    def category_params
+        params.require(:category).permit(
+            :name
+        )
+    end
 end
