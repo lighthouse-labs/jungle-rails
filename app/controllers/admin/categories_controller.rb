51a8a44fe23b3d42f33308304ced7a3c198d427e
Mythily <mythily.mudunuru@gmail.com>
2019-07-12 21:15:26 +0000

    http auth and store sensitive info in env

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-    http_basic_authenticate_with name: "Jungle", password: "book"
+    http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS']
 
     def index
         @categories = Category.all
