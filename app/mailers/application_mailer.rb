45287cab818d530378b79159546c9f836f3967a9
Mythily <mythily.mudunuru@gmail.com>
2019-07-12 20:29:49 +0000

    receipt mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
-  layout 'mailer'
+
+  def sample_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sample Email')
+  end
 end
