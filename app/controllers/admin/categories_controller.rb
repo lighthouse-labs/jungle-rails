f5ccc4f4beb51047b334a9635795901741263433
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-12 13:46:43 -0400

    :bug: admin basic auth fix

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
   def index
     @categories = Category.all
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,6 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
-
   def authenticate_user!
     redirect_to :new
   end
