bdcaceb8d73a77df67029b42b346a46ea1d7cad2
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-13 15:30:33 -0400

    review adds to product per correct logged in user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,14 +2,24 @@ class ReviewsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    @review = product.reviews.create!(review_params)
-    redirect_to "/products/#{product.id}"
+    @review = product.reviews.new(review_params)
+    if current_user
+      @review.user_id = current_user.id
+      if @review.save
+        redirect_to "/products/#{product.id}"
+      end 
+    else
+      redirect_to "/login"
+    end
   end
 
 private
 
   def review_params
-    params.require(:description, :rating)
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
   end
 
 end
