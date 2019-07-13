f1e5fa6c659e285f7f6401ed798e075be157e80d
Melanie <m.maddix@gmail.com>
2019-07-13 19:44:05 +0000

    validates and creates review records

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,27 @@
 class ReviewsController < ApplicationController
 
-def create
-  raise "Yay, I'm here!"
-end
+  def create
+
+    # get associated product
+    @product = Product.find_by(id: params[:product_id])
+
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.product_id = params[:product_id]
+    @review.save!
+    redirect_to product_path(@product)
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :product_id,
+      :description,
+      :rating,
+      :user_id
+    )
+  end
 
 end
