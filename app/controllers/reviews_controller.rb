28bc6c5f5e3a081ac41ee3cc7c91d457c2210c4d
Joanna Wood <caitlinjoannawood@gmail.com>
2019-07-13 17:42:15 -0400

    successful delete of review for reviews matching currently logged in user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,6 +13,13 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.find params[:id]
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
 private
 
   def review_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only: [:create]
+    resources :reviews, except: [:edit, :update, :show, :index, :new]
   end
 
   resources :categories, only: [:show]
