a5776b02c4a2d58802273724ae949dae54b1325e
Melanie <m.maddix@gmail.com>
2019-07-13 20:14:52 +0000

    adds functionality to delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
 
+  # make sure user is logged in
+  before_filter :authorize
+
   def create
 
-    # get associated product
     @product = Product.find_by(id: params[:product_id])
-
     @review = Review.new(review_params)
     @review.user_id = current_user.id
     @review.product_id = params[:product_id]
@@ -13,14 +14,25 @@ class ReviewsController < ApplicationController
 
   end
 
+  def destroy
+
+    @product = Product.find_by(id: params[:product_id])
+    @review = Review.find_by(id: params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+
+  end
+
   private
 
   def review_params
+
     params.require(:review).permit(
       :product_id,
       :description,
       :rating,
       :user_id
+
     )
   end
 
