120937086b877e426cbbe7846fc2ff9ee87c4c95
Mythily <mythily.mudunuru@gmail.com>
2019-07-15 20:07:31 +0000

    edit user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ActiveRecord::Base
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: { case_sensitive: false }
-    validates :password, length: { minimum: 4 }
-    validates :password_digest, presence: true
+    validates :password, length: { minimum: 4 }, presence: true
+    validates :password_confirmation, presence: true
 
-    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it 'should let a user be created if password and password_confirmation match'
     
-    it 'should '
+    it 'should not let a user be created if a user exists in system, not case sensitive'
+
   end
   
 end
